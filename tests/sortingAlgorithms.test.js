4ae8f2f72e4bb5076317631c53a2cab9e3aa3b8f
phothinh <phothinh01@gmail.com>
2024-04-24 14:00:03 +0200

    Tests sorts  w/ jest-sorted

--- tests/sortingAlgorithms.test.js
+++ tests/sortingAlgorithms.test.js
@@ -1,34 +1,40 @@
 const { bubbleSort, quickSort, selectionSort, insertionSort, mergeSort } = require('../src/sortingAlgorithms');
+const { isSorted } = require('jest-sorted');
 
 describe('Sorting Algorithms', () => {
   test('Bubble Sort', () => {
     const unsortedArray = [4, 2, 7, 1, 9];
     const sortedArray = bubbleSort(unsortedArray);
     expect(sortedArray).toEqual([1, 2, 4, 7, 9]);
+    expect(sortedArray).toBeSorted();
   });
 
   test('Quick Sort', () => {
     const unsortedArray = [4, 2, 7, 1, 9];
     const sortedArray = quickSort(unsortedArray);
     expect(sortedArray).toEqual([1, 2, 4, 7, 9]);
+    expect(sortedArray).toBeSorted();
   });
 
   test('Selection Sort', () => {
     const unsortedArray = [4, 2, 7, 1, 9];
     const sortedArray = selectionSort(unsortedArray);
     expect(sortedArray).toEqual([1, 2, 4, 7, 9]);
+    expect(sortedArray).toBeSorted();
   });
 
   test('Insertion Sort', () => {
     const unsortedArray = [4, 2, 7, 1, 9];
     const sortedArray = insertionSort(unsortedArray);
     expect(sortedArray).toEqual([1, 2, 4, 7, 9]);
+    expect(sortedArray).toBeSorted();
   });
 
   test('Merge Sort', () => {
     const unsortedArray = [4, 2, 7, 1, 9];
     const sortedArray = mergeSort(unsortedArray);
     expect(sortedArray).toEqual([1, 2, 4, 7, 9]);
+    expect(sortedArray).toBeSorted();
   });
 
 });
