32d7c2a74f89e7ad052dd08c25fd741ac2769bc5
phothinh <phothinh01@gmail.com>
2024-04-24 11:40:36 +0200

    Code : Selection Sort

--- src/sortingAlgorithms.js
+++ src/sortingAlgorithms.js
@@ -29,9 +29,31 @@ function quickSort(array) {
   
     return [...quickSort(left), pivot, ...quickSort(right)];
 }
+
+function selectionSort(array) {
+    const n = array.length;
+  
+    for (let i = 0; i < n - 1; i++) {
+      let minIndex = i;
+  
+      for (let j = i + 1; j < n; j++) {
+        if (array[j] < array[minIndex]) {
+          minIndex = j;
+        }
+      }
+  
+      if (minIndex !== i) {
+        [array[i], array[minIndex]] = [array[minIndex], array[i]]; // Swap elements
+      }
+    }
+  
+    return array;
+}
+  
   
 module.exports = {
     bubbleSort,
     quickSort,
+    selectionSort,
 };
   
