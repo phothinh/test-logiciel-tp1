766f5275bbe87b8ec7c504837c25e1e6aad1de09
phothinh <phothinh01@gmail.com>
2024-04-24 11:35:16 +0200

    Code : Quick Sort

--- src/sortingAlgorithms.js
+++ src/sortingAlgorithms.js
@@ -8,10 +8,30 @@ function bubbleSort(array) {
       }
     }
     return array;
-  }
-  
-  module.exports = {
-    bubbleSort,
+}
 
-  };
+function quickSort(array) {
+    if (array.length <= 1) {
+      return array;
+    }
+  
+    const pivot = array[0];
+    const left = [];
+    const right = [];
+  
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < pivot) {
+        left.push(array[i]);
+      } else {
+        right.push(array[i]);
+      }
+    }
+  
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
+  
+module.exports = {
+    bubbleSort,
+    quickSort,
+};
   
