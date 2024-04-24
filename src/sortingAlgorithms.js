461f32e9c843110cc282d6809ad8e47f39fb53ae
phothinh <phothinh01@gmail.com>
2024-04-24 11:27:13 +0200

    code : add bubbleSort function

---
+++ src/sortingAlgorithms.js
@@ -0,0 +1,17 @@
+function bubbleSort(array) {
+    const n = array.length;
+    for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        if (array[j] > array[j + 1]) {
+          [array[j], array[j + 1]] = [array[j + 1], array[j]];
+        }
+      }
+    }
+    return array;
+  }
+  
+  module.exports = {
+    bubbleSort,
+
+  };
+  
