f8b794e63737dc55b17a60e1915bc14425d916cf
phothinh <phothinh01@gmail.com>
2024-04-24 11:43:13 +0200

    Code : Insertion Sort

--- src/sortingAlgorithms.js
+++ src/sortingAlgorithms.js
@@ -49,11 +49,30 @@ function selectionSort(array) {
   
     return array;
 }
+
+function insertionSort(array) {
+    const n = array.length;
+  
+    for (let i = 1; i < n; i++) {
+      let current = array[i];
+      let j = i - 1;
+  
+      while (j >= 0 && array[j] > current) {
+        array[j + 1] = array[j];
+        j--;
+      }
+  
+      array[j + 1] = current;
+    }
+  
+    return array;
+}
   
   
 module.exports = {
     bubbleSort,
     quickSort,
     selectionSort,
+    insertionSort,
 };
   
