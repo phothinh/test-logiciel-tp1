b00422fd8ad7b048fedb1549e416d9127732482b
phothinh <phothinh01@gmail.com>
2024-04-24 11:38:18 +0200

    Test : Selection Sort

--- tests/sortingAlgorithms.test.js
+++ tests/sortingAlgorithms.test.js
@@ -13,4 +13,10 @@ describe('Sorting Algorithms', () => {
     expect(sortedArray).toEqual([1, 2, 4, 7, 9]);
   });
 
+  test('Selection Sort', () => {
+    const unsortedArray = [4, 2, 7, 1, 9];
+    const sortedArray = selectionSort(unsortedArray);
+    expect(sortedArray).toEqual([1, 2, 4, 7, 9]);
+  });
+
 });
